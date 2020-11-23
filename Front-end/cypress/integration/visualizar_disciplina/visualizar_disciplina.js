357a96f4d9672e1c83a186087f9e6c822e58ff8c
soutog <soutog4@gmail.com>
2020-11-23 18:13:28 -0300

    visualizar_disciplina_v2

--- Front-end/cypress/integration/visualizar_disciplina/visualizar_disciplina.js
+++ Front-end/cypress/integration/visualizar_disciplina/visualizar_disciplina.js
@@ -1,1 +1,20 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+
+Given("que é exibida a tela de visualização de disciplina")
+
+// editar disciplina
+When("clico em editar disciplina")
+
+Then("a tela de editar disciplina é exibida")
+
+// excluir disciplina
+When("clico em excluir disciplina")
+
+Then("é exibida a tela de listagem de disciplinas")
+
+And("a disciplina é deletada")
+
+//visualizar disciplina
+When("clico em uma atividade")
+
+Then("é exibida a tela de visualização da atividade")
