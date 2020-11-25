d71867b7d3d6e8ee6959efa4c8e8c698dd7d4f3e
soutog <soutog4@gmail.com>
2020-11-25 19:25:29 -0300

    visualizar_disciplina

--- Front-end/cypress/integration/visualizar_disciplina.feature
+++ Front-end/cypress/integration/visualizar_disciplina.feature
@@ -4,22 +4,7 @@ Funcionalidade: Visualiza uma disciplina
   Exibe as informações da disciplina
   juntamente com as opções de editar e excluir
 
-Cenário: Exibir disciplina
-  Quando a tela de visualizar disciplina é acessada
-  Então a disciplina é exibida
-
-Cenário: Editar disciplina
-  Dado que é exibida a tela de visualização de disciplina
-  Quando clico em editar disciplina
-  Então a tela de editar disciplina é exibida
-
-Cenário: Excluir disciplina
-  Dado que é exibida a tela de visualização de disciplina
-  Quando clico em excluir disciplina
-  Então é exibida a tela de listagem de disciplinas
-  E a disciplina é deletada
-
-Cenário: Visualizar atividade
-  Dado que é exibida a tela de visualização de disciplina
-  Quando clico em uma atividade
-  Então é exibida a tela de visualização da atividade
+  Cenário: Visualizar disciplina
+    Dado que é exibida a tela de visualização de disciplina
+    Quando clico em uma disciplina
+    Então a tela de editar e excluir a disciplina é exibida
--- Front-end/cypress/integration/visualizar_disciplina/visualizar_disciplina.js
+++ Front-end/cypress/integration/visualizar_disciplina/visualizar_disciplina.js
@@ -1,35 +1,27 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
 
 Given("que é exibida a tela de visualização de disciplina", () => {
+    
     cy.visit('./')
-
     cy.get('#usuarioLogin').type('soutog')
     cy.get('#usuarioSenha').type(1234)
-
     cy.get('#btnEntrar').click()
+    cy.url().should('eq', 'http://localhost:3000/home')
 
     cy.get('#minhasDisciplinas').click()
-}) 
+    cy.url().should('eq', 'http://localhost:3000/disciplinas')
+
+}); 
 
 
 When("clico em uma disciplina", () => {
+    cy.get('#nomeDisciplinaTabela').should('have.attr', 'href')
     cy.get('#nomeDisciplinaTabela').click()
+});
 
-    
-})
+Then("a tela de editar e excluir a disciplina é exibida", () => { 
+    cy.url().should('eq', 'http://localhost:3000/disciplinas/view/3LksGA_')
+});
 
-Then("a tela de editar disciplina é exibida", () =>{
-    cy.visit()
-})
-
-// // excluir disciplina
-// When("clico em excluir disciplina")
-
-// Then("é exibida a tela de listagem de disciplinas")
-
-// And("a disciplina é deletada")
-
-// //visualizar disciplina
-// When("clico em uma atividade")
-
-// Then("é exibida a tela de visualização da atividade")
+And('a disciplina criada está presente', () => {
+});
