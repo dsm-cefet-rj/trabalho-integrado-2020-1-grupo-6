4fe468f1e6d68060c139a223f8bcb9efcae5b5b7
bruna-capistrano <54087350+bruna-capistrano@users.noreply.github.com>
2020-11-25 00:25:11 -0300

    .gitignore is now working

--- Front-end/cypress/integration/listar_disciplinas/listar_disciplinas.js
+++ Front-end/cypress/integration/listar_disciplinas/listar_disciplinas.js
@@ -1,6 +1,7 @@
 // This will be a new feature implementation on cypress
 
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps";
+//import cy from "cypress"
 
 Given('que temos disciplinas cadastradas', async function (disciplinas) {
     this.disciplinas = disciplinas.hashes();
@@ -13,13 +14,33 @@ Given('que temos disciplinas cadastradas', async function (disciplinas) {
             id: parseInt(d.id)
         }
         this.disciplinas[i] = d_cast;
-        
+        await window.fetch('http://192.168.0.13:3000/disciplinas/' + d.id, {method: 'DELETE'})
+        await window.fetch('http://192.168.0.13:3000/disciplinas/', {method: 'POST', body: JSON.stringify(d_cast),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+        });
+
+        i++;
     }
 
 });
 
-When('clico em Minhas disciplinas na Home', () => cy.visit('./'));
+/*When('clico em Minhas disciplinas na Home', () => cy.visit('./disciplinas'));
 
 Then('as disciplinas são exibidas', function() {
+    for (let d of this.disciplinas){
+        //nome da disciplina
+        cy.get(d.id)
+            .contains(d.Nome)
+            .should('be.visible');
+
+        //dados da disciplina
+        cy.get(d.id)
+            .contains(d.status)
+            .should('be.visible')
+
+    }
     
-});
+});*/
