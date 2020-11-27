7a5e98f5b53cdbd4f11cd7a8a7058647c56010f9
soutog <soutog4@gmail.com>
2020-11-27 00:34:25 -0300

    teste defeito criar/editar disciplina

---
+++ Front-end/src/pages/EditarDisciplina/EditarDisciplina.test.js
@@ -0,0 +1,97 @@
+//####### CAMPO NOME ################################
+
+test('Nome vazio', () => {throw 'Not implemented yet'});
+
+test('Nome limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Nome válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Nome limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO PERIODO #############################
+
+test('Periodo vazio', () => {throw 'Not implemented yet'});
+
+test('Periodo limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Periodo válido', () => {throw 'Not implemented yet'});
+
+test('Periodo limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Periodo limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Periodo limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO HORARIO #############################
+
+test('Horario vazio', () => {throw 'Not implemented yet'});
+
+test('Horario limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Horario válido', () => {throw 'Not implemented yet'});
+
+test('Horario limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Horario limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Horario limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO LOCAL ###############################
+
+test('Local vazio', () => {throw 'Not implemented yet'});
+
+test('Local limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Local válido', () => {throw 'Not implemented yet'});
+
+test('Local limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Local limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Local limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO NOMEPROFESSOR #######################
+
+test('NomeProfessor vazio', () => {throw 'Not implemented yet'});
+
+test('NomeProfessor limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('NomeProfessor válido', () => {throw 'Not implemented yet'});
+
+test('NomeProfessor limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('NomeProfessor limite superior válido', () => {throw 'Not implemented yet'});
+
+test('NomeProfessor limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO MATERIAL ############################
+
+test('Material vazio', () => {throw 'Not implemented yet'});
+
+test('Material limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Material válido', () => {throw 'Not implemented yet'});
+
+test('Material limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Material limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Material limite superior inválido', () => {throw 'Not implemented yet'});
+
+//####### CAMPO STATUS ################################
+
+test('Status vazio', () => {throw 'Not implemented yet'});
+
+test('Status limite inferior válido', () => {throw 'Not implemented yet'});
+
+test('Status válido', () => {throw 'Not implemented yet'});
+
+test('Status limite superior válido -1', () => {throw 'Not implemented yet'});
+
+test('Status limite superior válido', () => {throw 'Not implemented yet'});
+
+test('Status limite superior inválido', () => {throw 'Not implemented yet'});
