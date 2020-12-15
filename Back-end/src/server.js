6cc98acaa6490d6ac595c44055b63df37d8c4412
Felipe Lago <felipe.lago@grupoaguasdobrasil.com.br>
2020-12-15 00:28:46 -0300

    Usuario- implementado modelo, rotas

---
+++ Back-end/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost/src", { useCreateIndex: true, useFindAndModify: false, useNewUrlParser: true, useUnifiedTopology: true }, () => { 
+    console.log('Connected to database'); 
+});
+
+const server = express();
+server.use(express.json());
+
+server.use(routes);
+
+server.listen("3001");
