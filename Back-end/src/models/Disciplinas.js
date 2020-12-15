0fce6fa37b9d509e1b8cd664f540ec03adf5c830
Felipe Lago <felipe.lago@grupoaguasdobrasil.com.br>
2020-12-15 17:57:10 -0300

    Criado rotas, modelo, controller de Disciplina no backend

--- Back-end/src/models/Disciplinas.js
+++ Back-end/src/models/Disciplinas.js
@@ -6,30 +6,52 @@ const disciplinasSchema = mongoose.Schema({
         type: String,
         required: true,
         unique: true,        
-        minLength: 20,
+        minLength: 2,
         maxLength: 25
     },
     periodo: {
         type: String,
         required: true,
         unique: false,        
-        minLength: 20,
-        maxLength: 25
+        minLength: 1,
+        maxLength: 2
     },
     horario: {
         type: String,
         required: true,
         unique: false,        
-        minLength: 20,
+        minLength: 2,
         maxLength: 25
     },
     local: {
         type: String,
         required: true,
         unique: false,        
-        minLength: 20,
+        minLength: 2,
         maxLength: 25
     },
+    professor: {
+        type: String,
+        required: true,
+        unique: false,        
+        minLength: 2,
+        maxLength: 25
+    },
+    material: {
+        type: String,
+        required: true,
+        unique: false,        
+        minLength: 2,
+        maxLength: 25
+    },
+    status: {
+        type: String,
+        required: true,
+        unique: false,        
+        minLength: 2,
+        maxLength: 25
+    },
+
     idUsuario:{
         type: mongoose.Types.ObjectId,
         ref: 'Usuarios',
