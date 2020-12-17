92ab37f9688ed057b1c5c506128a977ea614078a
luizavoliveira <luizavoliveira@gmail.com>
2020-12-17 00:02:03 -0300

    validação campos usuario

--- Back-end/src/models/Atividades.js
+++ Back-end/src/models/Atividades.js
@@ -1,75 +1,59 @@
-const mongoose = require('mongoose');
-const normalize = require('normalize-mongoose');
+const mongoose = require("mongoose");
+const normalize = require("normalize-mongoose");
 
 const atividadesSchema = mongoose.Schema({
-    nome: {
-        type: String,
-        required: true,
-        unique: true,        
-        minLength: 2,
-        maxLength: 25
-    },
-    dataEntrega: {
-        type: String,
-        required: true,
-        unique: true,        
-        minLength: 2,
-        maxLength: 25
-    },
-    pontuacaoMax: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 1,
-        maxLength: 3
-    },
-    status: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 2,
-        maxLength: 25
-    },
-    tipo: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 2,
-        maxLength: 25
-    },
-    descricao: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 2,
-        maxLength: 25
-    },
-    notaFinal: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 2,
-        maxLength: 25
-    },
-    arquivo: {
-        type: String,
-        required: true,
-        unique: false,        
-        minLength: 2,
-        maxLength: 25
-    },
-    idDisciplina:{
-        type: mongoose.Types.ObjectId,
-        ref: 'Disciplinas',
-        required: true
-    },
-    idUsuario:{
-        type: mongoose.Types.ObjectId,
-        ref: 'Usuarios',
-        required: true
-    }
+  nome: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  dataEntrega: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  pontuacaoMax: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  status: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  tipo: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  descricao: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  notaFinal: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  arquivo: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  idDisciplina: {
+    type: mongoose.Types.ObjectId,
+    ref: "Disciplinas",
+    required: true,
+  },
+  idUsuario: {
+    type: mongoose.Types.ObjectId,
+    ref: "Usuarios",
+    required: true,
+  },
 });
 
 atividadesSchema.plugin(normalize);
 
-module.exports = mongoose.model('Atividades', atividadesSchema);
+module.exports = mongoose.model("Atividades", atividadesSchema);
